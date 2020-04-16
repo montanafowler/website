0902cba3eab1c43125b620e5eb9593043f743346
montanafowler <montanafowler@yahoo.com>
2020-04-15 19:43:11 -0700

    added for_sale written dropdown and about link

--- custom-select.js
+++ custom-select.js
@@ -1,57 +1,91 @@
 
-function getYearFromIndex(index) {
-  if(index == 0) {
-    return "2017_to_present";
+function getYearFromIndex(index, useYear) {
+  if(useYear) {
+    if(index == 0) {
+      return "2017_to_present";
+    }
+    var year = 2016 + parseInt(index);
+    return year.toString();
   }
-  var year = 2016 + parseInt(index);
-  return year.toString();
+  if(index == 0) {
+    return "all";
+  }
+  if(index == 1) {
+    return "for_sale";
+  }
+  return "not_for_sale";
 }
 
-function getIndexFromYear(year) {
-  if(year == "2017_to_present") {
+function getIndexFromYear(year, useYear) {
+  if(useYear) {
+    if(year == "2017_to_present") {
+      return 0;
+    } 
+    return (parseInt(year) - 2016);
+  }
+  if (year == "all") {
     return 0;
-  } 
-  return (parseInt(year) - 2016);
+  }
+  if(year == "for_sale") {
+    return 1;
+  }
+  return 2;
+}
+
+function getSaleValueFromIndex(index) {
+  if(index == 0) {
+    return "all";
+  } else if(index == 1) {
+    return "for_sale";
+  }
+  return "not_for_sale";
+}
+
+function getIndexFromSaleValue(saleValue) {
+  if(saleValue == "all") {
+    return 0;
+  } else if(saleValue == "for_sale") {
+    return 1;
+  }
+  return 2;
 }
 
 
 var x, i, j, selElmnt, a, b, c, value, span, year;
 console.log("custom-select.js");
+var useYear = true;
 /*look for any elements with the class "custom-select":*/
 x = document.getElementsByClassName("custom-select");
 for (i = 0; i < x.length; i++) {
   selElmnt = x[i].getElementsByTagName("select")[0];
+  console.log(x[i].id);
+  if (x[i].id == "saleDropdownMenu") {
+    useYear = false;
+  }
+
   /*for each element, create a new DIV that will act as the selected item:*/
   a = document.createElement("DIV");
   a.setAttribute("class", "select-selected");
   value = selElmnt.options[selElmnt.selectedIndex].value;
-  console.log("value", value);
-  year = getYearFromIndex(value);
-  console.log("year", year);
+  year = getYearFromIndex(value, useYear);
   span = '<span><img style=" display: inline-block; height: 25px; width: auto;" src="images/written_words/written_words_' + year + '.png" /></span>';
-  console.log("span", span);
   a.innerHTML = span;//selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  console.log("selElmnt.options[selElmnt.selectedIndex]", selElmnt.options[selElmnt.selectedIndex]);
   x[i].appendChild(a);
   /*for each element, create a new DIV that will contain the option list:*/
   b = document.createElement("DIV");
   b.setAttribute("class", "select-items select-hide");
   b.style = "background-color: yellow;";
-  // span = '<span><img style="display: inline-block; height: 25px; width: auto;" src="images/written_words/written_words_' + year + '.png" /></span>';
-  // b.innerHTML = span;
+
   for (j = 0; j < selElmnt.length; j++) {
     /*for each option in the original select element,
     create a new DIV that will act as an option item:*/
     c = document.createElement("DIV");
     c.style = "background-color: white;";
     c.innerHTML = selElmnt.options[j].innerHTML;
-    console.log("selElmnt.options[j]", selElmnt.options[j]);
     value = selElmnt.options[j].value;
-    year = getYearFromIndex(value);
-    console.log("year", year);
+    year = getYearFromIndex(value, useYear);
     span = '<span><img style="display: inline-block; height: 25px; width: auto;" src="images/written_words/written_words_' + year + '.png" /></span>';
     c.innerHTML = span;
-    //console.log("value2", value);
     c.addEventListener("click", function(e) {
         /*when an item is clicked, update the original select box,
         and the selected item:*/
@@ -59,15 +93,9 @@ for (i = 0; i < x.length; i++) {
         s = this.parentNode.parentNode.getElementsByTagName("select")[0];
         h = this.parentNode.previousSibling;
         for (i = 0; i < s.length; i++) {
-          console.log("this.innerHTML", this.innerHTML);
-          console.log("s.options[i].innerHTML", s.options[i].innerHTML);
-          console.log("s.options[i].value", value);
           value = s.options[i].value;
-          year = getYearFromIndex(value);
-          console.log("year", year);
+          year = getYearFromIndex(value, useYear);
           filename = "written_words_" + year + ".png";
-          console.log("filename", filename);
-          console.log("this.innerHTML.includes(filename)", this.innerHTML.includes(filename));
           if (this.innerHTML.includes(filename)) {
             s.selectedIndex = i;
             h.innerHTML = this.innerHTML;
@@ -99,13 +127,7 @@ function closeAllSelect(elmnt) {
   var x, y, i, arrNo = [];
   x = document.getElementsByClassName("select-items");
   y = document.getElementsByClassName("select-selected");
-  console.log("x", x);
-  console.log("y", y);
-  console.log('y["2017_to_present"]', y["2017_to_present"]);
   for (i = 0; i < y.length; i++) {
-    console.log("y[i]", y[i]);
-    console.log("y[i].innerHTML", y[i].innerHTML);
-    console.log("elmnt.innerHTML", elmnt.innerHTML);
     if (elmnt == y[i]) {
       arrNo.push(i);
     } else {
@@ -113,7 +135,6 @@ function closeAllSelect(elmnt) {
     }
   }
   for (i = 0; i < x.length; i++) {
-    console.log("x[i].innerHTML", x[i].innerHTML);
     if (arrNo.indexOf(i)) {
       x[i].classList.add("select-hide");
     }
